449b4302de69b2b14c3ab29b7426e2aecf9d5965
Alejandro Caro <alehu2003@gmail.com>
2026-07-29 04:31:04 +0200

    Translated using Weblate (Uzbek)
    Currently translated at 2.2% (10 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/uz/

--- dev-tools/i18n/release/uz.ts
+++ dev-tools/i18n/release/uz.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/About/About.qml" line="44"/>
         <source>开源、免费的离线OCR软件</source>
-        <translation type="unfinished"></translation>
+        <translation>Ochiq kodli, bepul, oflayn OCR dasturi</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/About/About.qml" line="50"/>
