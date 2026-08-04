8b80705858b98bda87db6b5fd65a3d7f6f8d4e22
Alejandro Caro <alehu2003@gmail.com>
2026-08-04 20:08:23 +0200

    Translated using Weblate (Kabyle)
    Currently translated at 51.1% (232 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/kab/

--- dev-tools/i18n/release/kab.ts
+++ dev-tools/i18n/release/kab.ts
@@ -685,12 +685,13 @@ Tabdart n tenumeriwin i %2 d tilemt.</translation>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="487"/>
         <source>显示更多高级选项。它们标有 * 号。
 请谨慎修改高级选项。</source>
-        <translation type="unfinished"></translation>
+        <translation>Sken ugar n tnefrunin tineqqurin. Ccaren s uzmul (*).
+Beddel tinefrunin tineqqurin s lḥader.</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="664"/>
         <source>展开</source>
-        <translation type="unfinished"></translation>
+        <translation>Snefli</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="664"/>
@@ -700,7 +701,7 @@ Tabdart n tenumeriwin i %2 d tilemt.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="802"/>
         <source>必须为整数</source>
-        <translation type="unfinished"></translation>
+        <translation>Ilaq ad yili d amḍan ummid</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="806"/>
