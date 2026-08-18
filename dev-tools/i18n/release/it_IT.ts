c6cb295449260269ea4e9c0c37d7870ba396b075
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-08-18 14:59:00 +0200

    Translated using Weblate (Italian)
    Currently translated at 8.3% (38 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/it/

--- dev-tools/i18n/release/it_IT.ts
+++ dev-tools/i18n/release/it_IT.ts
@@ -202,12 +202,12 @@ Terminare l&apos;attività e chiudere la pagina?</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/BatchDOC/BatchDOCConfigs.qml" line="47"/>
         <source>若一页文档既存在图片又存在文本，如何进行处理</source>
-        <translation type="unfinished"></translation>
+        <translation>Come elaborare una pagina del documento che contiene sia immagini che testo</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/BatchDOC/BatchDOCConfigs.qml" line="49"/>
         <source>混合OCR/原文本</source>
-        <translation type="unfinished"></translation>
+        <translation>OCR misto / testo originale</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/BatchDOC/BatchDOCConfigs.qml" line="50"/>
