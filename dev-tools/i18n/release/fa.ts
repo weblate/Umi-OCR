acf7059abee898b9d3b136289ab159b9fccac1a9
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-08-18 14:58:53 +0200

    Translated using Weblate (Persian)
    Currently translated at 8.3% (38 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/fa/

--- dev-tools/i18n/release/fa.ts
+++ dev-tools/i18n/release/fa.ts
@@ -202,12 +202,12 @@
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/BatchDOC/BatchDOCConfigs.qml" line="47"/>
         <source>若一页文档既存在图片又存在文本，如何进行处理</source>
-        <translation type="unfinished"></translation>
+        <translation>اگر یک صفحه از سند هم تصویر و هم متن داشته باشد، چگونه پردازش می‌شود</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/BatchDOC/BatchDOCConfigs.qml" line="49"/>
         <source>混合OCR/原文本</source>
-        <translation type="unfinished"></translation>
+        <translation>OCR ترکیبی / متن اصلی</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/BatchDOC/BatchDOCConfigs.qml" line="50"/>
