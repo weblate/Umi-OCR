516c0c42cb5f3f2ce39028918248183f9eaf2aba
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-08-18 14:40:14 +0200

    Translated using Weblate (Bengali)
    Currently translated at 8.5% (39 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/bn/

--- dev-tools/i18n/release/bn.ts
+++ dev-tools/i18n/release/bn.ts
@@ -202,12 +202,12 @@
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/BatchDOC/BatchDOCConfigs.qml" line="47"/>
         <source>若一页文档既存在图片又存在文本，如何进行处理</source>
-        <translation type="unfinished"></translation>
+        <translation>একটি নথির পাতায় ছবি ও পাঠ্য উভয়ই থাকলে কীভাবে প্রক্রিয়া করা হবে</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/BatchDOC/BatchDOCConfigs.qml" line="49"/>
         <source>混合OCR/原文本</source>
-        <translation type="unfinished"></translation>
+        <translation>মিশ্র OCR / মূল পাঠ্য</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/BatchDOC/BatchDOCConfigs.qml" line="50"/>
