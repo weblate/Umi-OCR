1da77a23f4d54454b9a9923c077fe9e53c13fa89
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-09-05 23:20:10 +0200

    Translated using Weblate (Turkish)
    Currently translated at 29.9% (136 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/tr/

--- dev-tools/i18n/release/tr.ts
+++ dev-tools/i18n/release/tr.ts
@@ -733,27 +733,27 @@ Gelişmiş seçenekleri dikkatli bir şekilde değiştirin.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1133"/>
         <source>更新热键成功</source>
-        <translation type="unfinished"></translation>
+        <translation>Klavye kısayolu başarıyla güncellendi</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1133"/>
         <source>%1的快捷键为 %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 için klavye kısayolu %2&apos;dir</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1141"/>
         <source>%1 快捷键%2已被注册，请尝试另外的按键组合。</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 için %2 klavye kısayolu zaten kayıtlı, lütfen başka bir tuş kombinasyonu deneyin.</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1144"/>
         <source>%1 快捷键%2无法注册，请尝试另外的按键组合。</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 için %2 klavye kısayolu kaydedilemedi, lütfen başka bir tuş kombinasyonu deneyin.</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1153"/>
         <source>请按下快捷键组合。按【Esc】退出。</source>
-        <translation type="unfinished"></translation>
+        <translation>Klavye kısayolu kombinasyonuna basın. Çıkmak için 【Esc】 tuşuna basın.</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1166"/>
