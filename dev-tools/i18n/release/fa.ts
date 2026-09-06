6509acd45f3ea84db5c4284101ed37fce1e9af29
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-09-07 01:45:16 +0200

    Translated using Weblate (Persian)
    Currently translated at 31.9% (145 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/fa/

--- dev-tools/i18n/release/fa.ts
+++ dev-tools/i18n/release/fa.ts
@@ -784,13 +784,15 @@
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="211"/>
         <source>界面字体：
 软件中大部分UI的字体。</source>
-        <translation type="unfinished"></translation>
+        <translation>فونت رابط کاربری:
+فونت بیشتر بخش‌های رابط کاربری نرم‌افزار.</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="219"/>
         <source>内容字体：
 识别结果内容的字体。</source>
-        <translation type="unfinished"></translation>
+        <translation>فونت محتوا:
+فونت محتوای نتایج ocr.</translation>
     </message>
 </context>
 <context>
@@ -798,17 +800,17 @@
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="20"/>
         <source>快捷方式</source>
-        <translation type="unfinished"></translation>
+        <translation>میانبر</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="24"/>
         <source>桌面</source>
-        <translation type="unfinished"></translation>
+        <translation>دسکتاپ</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="32"/>
         <source>开始菜单</source>
-        <translation type="unfinished"></translation>
+        <translation>منوی شروع</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="40"/>
