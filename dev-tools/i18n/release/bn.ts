ab667f8a11b77ee484894a72e65997365dda812b
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-09-07 01:11:48 +0200

    Translated using Weblate (Bengali)
    Currently translated at 31.2% (142 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/bn/

--- dev-tools/i18n/release/bn.ts
+++ dev-tools/i18n/release/bn.ts
@@ -758,12 +758,12 @@
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1166"/>
         <source>当前快捷键录制已在进行，不能同时录制！</source>
-        <translation type="unfinished"></translation>
+        <translation>শর্টকাট রেকর্ডিং ইতিমধ্যে চলছে, একই সময়ে একাধিক শর্টকাট রেকর্ড করা যায় না!</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1168"/>
         <source>无法录制快捷键</source>
-        <translation type="unfinished"></translation>
+        <translation>শর্টকাট রেকর্ড করা যায়নি</translation>
     </message>
 </context>
 <context>
@@ -771,7 +771,7 @@
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Widgets/DropArea_.qml" line="11"/>
         <source>松手放入文件</source>
-        <translation type="unfinished"></translation>
+        <translation>ছেড়ে দিয়ে ফাইলটি রাখুন</translation>
     </message>
 </context>
 <context>
@@ -779,12 +779,12 @@
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="107"/>
         <source>界面</source>
-        <translation type="unfinished"></translation>
+        <translation>ইন্টারফেস</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="115"/>
         <source>内容</source>
-        <translation type="unfinished"></translation>
+        <translation>বিষয়বস্তু</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="211"/>
