b5971310e8bafe34f074d7ef530eae8ba3cccc0e
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-09-07 01:46:35 +0200

    Translated using Weblate (Vietnamese)
    Currently translated at 34.1% (155 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/vi/

--- dev-tools/i18n/release/vi.ts
+++ dev-tools/i18n/release/vi.ts
@@ -784,19 +784,21 @@ Hãy thận trọng khi thay đổi các tùy chọn nâng cao.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="115"/>
         <source>内容</source>
-        <translation type="unfinished"></translation>
+        <translation>Nội dung</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="211"/>
         <source>界面字体：
 软件中大部分UI的字体。</source>
-        <translation type="unfinished"></translation>
+        <translation>Phông chữ giao diện:
+Phông chữ của hầu hết các thành phần UI trong phần mềm.</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="219"/>
         <source>内容字体：
 识别结果内容的字体。</source>
-        <translation type="unfinished"></translation>
+        <translation>Phông chữ nội dung:
+Phông chữ của nội dung kết quả ocr.</translation>
     </message>
 </context>
 <context>
@@ -804,17 +806,17 @@ Hãy thận trọng khi thay đổi các tùy chọn nâng cao.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="20"/>
         <source>快捷方式</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiếng Việt: Phím tắt</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="24"/>
         <source>桌面</source>
-        <translation type="unfinished"></translation>
+        <translation>Màn hình nền</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="32"/>
         <source>开始菜单</source>
-        <translation type="unfinished"></translation>
+        <translation>Menu Bắt đầu</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="40"/>
