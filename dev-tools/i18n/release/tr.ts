60977a7a0d629ebd45095d2499c9981c422248a0
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-09-07 01:45:56 +0200

    Translated using Weblate (Turkish)
    Currently translated at 32.1% (146 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/tr/

--- dev-tools/i18n/release/tr.ts
+++ dev-tools/i18n/release/tr.ts
@@ -790,13 +790,15 @@ Gelişmiş seçenekleri dikkatli bir şekilde değiştirin.</translation>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="211"/>
         <source>界面字体：
 软件中大部分UI的字体。</source>
-        <translation type="unfinished"></translation>
+        <translation>Arayüz yazı tipi:
+Yazılımın çoğu kullanıcı arayüzünün yazı tipi.</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="219"/>
         <source>内容字体：
 识别结果内容的字体。</source>
-        <translation type="unfinished"></translation>
+        <translation>İçerik yazı tipi:
+ocr sonuçlarının içerik yazı tipi.</translation>
     </message>
 </context>
 <context>
@@ -804,17 +806,17 @@ Gelişmiş seçenekleri dikkatli bir şekilde değiştirin.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="20"/>
         <source>快捷方式</source>
-        <translation type="unfinished"></translation>
+        <translation>Türkçe: Kısayol</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="24"/>
         <source>桌面</source>
-        <translation type="unfinished"></translation>
+        <translation>Masaüstü</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="32"/>
         <source>开始菜单</source>
-        <translation type="unfinished"></translation>
+        <translation>Başlat menüsü</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="40"/>
