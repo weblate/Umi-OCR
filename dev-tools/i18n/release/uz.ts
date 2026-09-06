a7304e20d54d1581aa900376c500bbd715a96338
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-09-07 01:46:23 +0200

    Translated using Weblate (Uzbek)
    Currently translated at 34.1% (155 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/uz/

--- dev-tools/i18n/release/uz.ts
+++ dev-tools/i18n/release/uz.ts
@@ -790,13 +790,15 @@ Kengaytirilgan parametrlarni ehtiyotkorlik bilan o‘zgartiring.</translation>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="211"/>
         <source>界面字体：
 软件中大部分UI的字体。</source>
-        <translation type="unfinished"></translation>
+        <translation>O‘zbekcha: Interfeys shrifti:
+Dasturdagi aksariyat UI elementlarining shrifti.</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="219"/>
         <source>内容字体：
 识别结果内容的字体。</source>
-        <translation type="unfinished"></translation>
+        <translation>O‘zbekcha: Kontent shrifti:
+ocr natijalari kontentining shrifti.</translation>
     </message>
 </context>
 <context>
@@ -804,17 +806,17 @@ Kengaytirilgan parametrlarni ehtiyotkorlik bilan o‘zgartiring.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="20"/>
         <source>快捷方式</source>
-        <translation type="unfinished"></translation>
+        <translation>O‘zbekcha: Yorliq</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="24"/>
         <source>桌面</source>
-        <translation type="unfinished"></translation>
+        <translation>Ish stoli</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="32"/>
         <source>开始菜单</source>
-        <translation type="unfinished"></translation>
+        <translation>Boshlash menyusi</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/GlobalConfigs.qml" line="40"/>
