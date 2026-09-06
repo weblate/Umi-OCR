b99547f84d6a469d29fc17cd2de289c59142205d
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-09-07 01:10:33 +0200

    Translated using Weblate (Vietnamese)
    Currently translated at 32.8% (149 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/vi/

--- dev-tools/i18n/release/vi.ts
+++ dev-tools/i18n/release/vi.ts
@@ -758,12 +758,12 @@ Hãy thận trọng khi thay đổi các tùy chọn nâng cao.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1166"/>
         <source>当前快捷键录制已在进行，不能同时录制！</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang ghi phím tắt, không thể ghi đồng thời nhiều phím tắt!</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1168"/>
         <source>无法录制快捷键</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ghi phím tắt</translation>
     </message>
 </context>
 <context>
@@ -771,7 +771,7 @@ Hãy thận trọng khi thay đổi các tùy chọn nâng cao.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Widgets/DropArea_.qml" line="11"/>
         <source>松手放入文件</source>
-        <translation type="unfinished"></translation>
+        <translation>Thả tay để thêm tệp</translation>
     </message>
 </context>
 <context>
@@ -779,7 +779,7 @@ Hãy thận trọng khi thay đổi các tùy chọn nâng cao.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="107"/>
         <source>界面</source>
-        <translation type="unfinished"></translation>
+        <translation>Giao diện</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="115"/>
