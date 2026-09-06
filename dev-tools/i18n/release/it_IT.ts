a0176e9cb3db4a87f9dc3316592b6a5a125c1553
Alejandro Hugo Caro Borba <alehu35@outlook.com>
2026-09-07 01:12:31 +0200

    Translated using Weblate (Italian)
    Currently translated at 31.0% (141 of 454 strings)
    
    Translation: Umi-OCR/UI
    Translate-URL: https://hosted.weblate.org/projects/umi-ocr/ui/it/

--- dev-tools/i18n/release/it_IT.ts
+++ dev-tools/i18n/release/it_IT.ts
@@ -758,12 +758,12 @@ Modifica le opzioni avanzate con cautela.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1166"/>
         <source>当前快捷键录制已在进行，不能同时录制！</source>
-        <translation type="unfinished"></translation>
+        <translation>La registrazione del tasto di scelta rapida è già in corso, non è possibile registrare più tasti contemporaneamente!</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/Configs.qml" line="1168"/>
         <source>无法录制快捷键</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossibile registrare il tasto di scelta rapida</translation>
     </message>
 </context>
 <context>
@@ -771,7 +771,7 @@ Modifica le opzioni avanzate con cautela.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Widgets/DropArea_.qml" line="11"/>
         <source>松手放入文件</source>
-        <translation type="unfinished"></translation>
+        <translation>Rilascia per inserire il file</translation>
     </message>
 </context>
 <context>
@@ -779,12 +779,12 @@ Modifica le opzioni avanzate con cautela.</translation>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="107"/>
         <source>界面</source>
-        <translation type="unfinished"></translation>
+        <translation>Interfaccia</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="115"/>
         <source>内容</source>
-        <translation type="unfinished"></translation>
+        <translation>Contenuto</translation>
     </message>
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/GlobalConfigsPage/FontPanel.qml" line="211"/>
